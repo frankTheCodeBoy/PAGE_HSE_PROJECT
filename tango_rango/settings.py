8aeb92dc7c5a70c72d81388e5e1e4627ecbe1e42
frankTheCodeBoy <olumfrank48@gmail.com>
2021-02-27 15:15:37 +0300

    refinement

--- tango_rango/settings.py
+++ tango_rango/settings.py
@@ -140,6 +140,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-import dj_database_url
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+import django_heroku
+django_heroku.settings(locals())
